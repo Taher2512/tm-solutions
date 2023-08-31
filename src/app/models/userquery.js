9fc0a892cdf5c24163e23dd9ea9217d520871e7a
Blaze5333 <121744504+Blaze5333@users.noreply.github.com>
2023-09-01 00:28:32 +0530

    Update userquery.js

--- src/app/models/userquery.js
+++ src/app/models/userquery.js
@@ -3,7 +3,8 @@ const model=new Schema({
     name:String,
     email:String,
     subject:String,
-    description:String
+    description:String,
+    date:String
 })
 const UserQuery=mongoose.models.userQuery||mongoose.model('userQuery',model)
 export default UserQuery
